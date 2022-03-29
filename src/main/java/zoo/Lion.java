5d92d2466f8a42d6e866a146f4eebf97bfb8bc0c
ckhemani <chandru.khemani@gmail.com>
2022-03-29 15:08:42 -0400

    Added Lion Class

--- src/main/java/zoo/Lion.java
+++ src/main/java/zoo/Lion.java
@@ -1,4 +1,27 @@
 package zoo;
 
-public class Lion {
+public class Lion extends Animal{
+    private boolean isWild;
+
+    public Lion(int age, String gender, String name, boolean isWild) {
+        super(age, gender, name);
+        this.isWild = isWild;
+    }
+
+    public boolean isWild() {
+        return isWild;
+    }
+
+    public void setWild(boolean wild) {
+        isWild = wild;
+    }
+
+    @Override
+    public String toString() {
+        return "Lion{" +
+                "isWild=" + isWild +
+                '}';
+    }
 }
+
+
--- src/main/java/zoo/ZooMain.java
+++ src/main/java/zoo/ZooMain.java
@@ -1,7 +1,16 @@
 package zoo;
 
-public class ZooMain {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class ZooMain {
+    //Class Fields
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Zoo zoo = new Zoo("Chandru Zoo","11000 American Dream Way, Reston, VA");
+    private static final Lion lion = new Lion(12,"Male","test",true);
+    private static final Duck duck = new Duck(1,"Male","Donald");
+    private static final Fish fish = new Fish(1,"Male","Flapper",25,true);
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Zoo");
     }
 }
